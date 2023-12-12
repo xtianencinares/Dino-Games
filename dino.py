90f59c9bf6dade58dad459086b58ab30d062a817
Christian Encinares Bocz <144127538+xtianencinares@users.noreply.github.com>
2023-12-12 14:34:59 +0800

    Update dino.py

--- dino.py
+++ dino.py
@@ -15,7 +15,7 @@ white_color = (255,255,255)
 bg_color = (235, 235, 235)
 
  highest_scores = 0
-
+ 
 screen_layout_display = pygame.display.set_mode(screen_size_display) 
 time_clock = pygame.time.Clock()
 pygame.display.set_caption("Dino Run ")
